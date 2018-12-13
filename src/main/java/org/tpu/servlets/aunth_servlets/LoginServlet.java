23ca561ab17653a4b0285d8fb38a0ffd9221903d
GrigUP <grig_95@icloud.com>
2018-12-13 20:18:38 +0700

    last update for mac version

--- src/main/java/org/tpu/servlets/aunth_servlets/LoginServlet.java
+++ src/main/java/org/tpu/servlets/aunth_servlets/LoginServlet.java
@@ -18,6 +18,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.sendRedirect(req.getContextPath() + "/");
+
     }
 
     @Override
